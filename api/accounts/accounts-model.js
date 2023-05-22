4e94fc6fbccd4b9a89c8ba79e25fc1cb685f3dc3
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-05-22 23:05:31 +0300

    Model done

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,35 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // KODLAR BURAYA
-}
+  // KODLAR BURAYA    //ALSO SQL CODES NEARBY FOR SQL CODE EXPLANATION
+  return db("accounts"); // select * from accounts
+};
 
-const getById = id => {
+const getById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("id", id).first(); // select * from accounts where id = "demanded id (örn:1)" limit 1
+};
 
-const create = account => {
-  // KODLAR BURAYA
-}
+const getByName = (name) => {
+  return db("accounts").where("name", name).first(); // select * from accounts where name = "demanded name (örn: ali)" limit 1
+};
 
-const updateById = (id, account) => {
+const create = async (account) => {
   // KODLAR BURAYA
-}
+  const inserted = await db("accounts").insert(account); // insert into accounts values (account)
+  return getById(inserted[0]);
+};
 
-const deleteById = id => {
+const updateById = async (id, account) => {
   // KODLAR BURAYA
-}
+  await db("accounts").where("id", id).update(account); // update accounts set name=account.name, budget=account.budget where id=id
+  return getById(id);
+};
+
+const deleteById = (id) => {
+  // KODLAR BURAYA
+  return db("accounts").where("id", id).del(); // delete accounts where id=id
+};
 
 module.exports = {
   getAll,
@@ -24,4 +37,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+  getByName,
+};
